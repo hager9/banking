6d11aa4b6ae3b2b48faecd71f08674fa2d168ee7
hager9 <hm4500176@gmail.com>
2024-06-28 01:47:08 +0300

    modify next config to supress ts during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
     typescript: {
-      ignoreBuildErrors: true
-    },
-    eslint: {
-      ignoreDuringBuilds: true
-    }
-  };
+        ignoreBuildErrors: true
+      },
+      eslint: {
+        ignoreDuringBuilds: true
+      }
+};
+
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
